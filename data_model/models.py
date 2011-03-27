808929c67252ad8a2a7199f6f83168b5f7f12dc3
RedLion973 <red_lion-973@hotmail.fr>
2011-03-27 21:48:52 +0200

    Implements a data model creator app

---
+++ data_model/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+class Entity(models.Model):
+    name = models.CharField(u'Name', max_length=255)
+    related_entities = models.ManyToManyField('self', verbose_name=u'Related Entities')
+    
+    def __unicode__(self):
+        return u"%s" % (self.name)
+
+class Type(models.Model):
+    description = models.CharField(u'Description', max_length=255, unique=True)
+    
+    def __unicode__(self):
+        return u"%s" % (self.description)
+
+class EntityField(models.Model):
+    entity = models.ForeignKey(Entity, verbose_name=u'Entity')
+    name = models.CharField(u'Name', max_length=255)
+    type = models.ForeignKey(Type, verbose_name=u'Type')
+    size = models.PostiveInteger(u'Size')
+    null = models.BooleanField(u'Null', default=0)
+
+    def print_is_null(self):
+        if 0 == self.null:
+            return "not null"
+        else:
+            return "null"
+
+    def __unicode__(self):
+        return u"%s %s(%d) | %s" % (self.name, self.type, self.size, self.print_is_null)
+
+
